e605124329296fb404de6a616255baf3bfe7c6a4
Dennis <dennisat@users.noreply.github.com>
2020-11-08 10:05:32 +0100

    Build

---
+++ dist/interfaces.d.ts
@@ -0,0 +1,16 @@
+import { EColor } from "dyna-ui-styles";
+export declare enum EStyle {
+    ROUNDED = "ROUNDED",
+    FLATTED = "FLATTED"
+}
+export { EColor };
+export declare enum ETooltipDirection {
+    NORTH = "NORTH",
+    EAST = "EAST",
+    SOUTH = "SOUTH",
+    WEST = "WEST",
+    NORTH_EAST = "NORTH_EAST",
+    NORTH_WEST = "NORTH_WEST",
+    SOUTH_EAST = "SOUTH_EAST",
+    SOUTH_WEST = "SOUTH_WEST"
+}
